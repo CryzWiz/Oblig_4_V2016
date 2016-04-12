4528ec48e313eb9b3016ff52c6b50a74a3d30636
Allan Arnesen <allan.arnesen@gmail.com>
2016-04-12 12:26:52 +0200

    La til de korrekte klassenavnene og la til TowerOfHanoi for oppgave 2.

--- Oblig_4_V2016/src/oppgave_1/SnuTekst.java
+++ Oblig_4_V2016/src/oppgave_1/SnuTekst.java
@@ -1,10 +1,13 @@
 package oppgave_1;
 
-public class Oppgave_1 {
+public class SnuTekst {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+	}
+	public static void baklengs(String tekst){
+		
 	}
 
 }
