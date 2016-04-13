0dfa232f69c70a1ba1d31024ae0c7f38d93d5e49
Allan Arnesen <allan.arnesen@gmail.com>
2016-04-13 13:35:27 +0200

    V0.1

--- Oblig_4_V2016/src/oppgave_1/SnuTekst.java
+++ Oblig_4_V2016/src/oppgave_1/SnuTekst.java
@@ -1,13 +1,21 @@
 package oppgave_1;
-
+import java.util.*;
 public class SnuTekst {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		System.out.println("Legg inn setningen/ordet du ønsker å snu:");
+		String tekst = input.nextLine();
+		baklengs(tekst);
 
 	}
 	public static void baklengs(String tekst){
-		
+		int n = tekst.length() - 1;
+		System.out.println("Setningen du skrev inn baklengs:");
+		while(n >= 0){
+		System.out.print(tekst.charAt(n));
+		n--;
+		}
 	}
 
 }
